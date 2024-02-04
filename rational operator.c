78ff9856261c7d88eae7206fa05e96b193881e8d
Zarif Hasan <133641395+Zarif-hasann@users.noreply.github.com>
2024-02-04 12:43:03 +0600

    Add files via upload

---
+++ rational operator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+ // ralational operator
+  int a=20, b=5;
+  printf("%d", a >  b);
+  printf("%d", a >= b);
+  printf("%d", a <  b);
+  printf("%d", a <= b);
+  printf("%d", a ==b);
+  printf("%d", a != b);
+    return 0;
+}
